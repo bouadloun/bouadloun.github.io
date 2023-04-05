d76af415e3c526c886d67ac68ec011528b70047b
Alexandra Strang <bouadloun1@gmail.com>
2023-04-05 09:45:47 +0200

    2nd day

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function onStartButtonClick() {
+  document.getElementById("menu").classList.add("hidden");
 
+  document.getElementById("game").classList.remove("hidden");
+}
+
+document.getElementById("play-button").onclick = onStartButtonClick;
+
+function onPlayAgainButtonClick() {
+  document.getElementById("game-over").classList.add("hidden");
+
+  document.getElementById("game").classList.remove("hidden");
+}
+document.getElementById("play-again-button").onclick = onPlayAgainButtonClick;
+
+function backToHomeButtonClick() {
+  document.getElementById("game-won").classList.add("hidden");
+
+  document.getElementById("menu").classList.remove("hidden");
+}
+
+document.getElementById("back-home-button").onclick = backToHomeButtonClick;
+
+// const element = document.getElementById("back-home-button")
+// element.addEventListener("click", backToHomeButtonClick);
