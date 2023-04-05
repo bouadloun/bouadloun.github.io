cd0873f58aae3585a374722a574ec35f88cd61c1
Alexandra Strang <bouadloun1@gmail.com>
2023-04-05 13:47:36 +0200

    added javascript

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function onStartButtonClick() {
   document.getElementById("menu").classList.add("hidden");
 
   document.getElementById("game").classList.remove("hidden");
+  startRound();
 }
 
 document.getElementById("play-button").onclick = onStartButtonClick;
@@ -23,3 +24,30 @@ document.getElementById("back-home-button").onclick = backToHomeButtonClick;
 
 // const element = document.getElementById("back-home-button")
 // element.addEventListener("click", backToHomeButtonClick);
+
+const colors = ["Red", "Green", "Blue", "Pink", "Yellow", "Purple"];
+
+function startRound() {
+  let chosen_index = getRandomInt(colors.length);
+  const chosenColor = colors[chosen_index];
+
+  chosen_index = getRandomInt(colors.length);
+  const chosenText = colors[chosen_index];
+  const htmlElement = document.getElementById("random-word");
+  htmlElement.style.color = chosenColor;
+  htmlElement.innerHTML = chosenText;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function onAnswerClick(event) {
+  const expectedColor = document.getElementById("random-word").style.color;
+  const buttonString = event.target.innerHTML;
+
+  if (expectedColor === buttonString) {
+    // console.log("you win");
+  }
+}
+document.getElementsByClassName("buttons").onclick = onAnswerClick;
